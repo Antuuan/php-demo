5a0fd4788414ce8a7bc627dcabd8b669533b0984
Antuuan <antonycheng2007@gmail.com>
2024-10-15 09:37:48 +0100

    uesg

---
+++ adder0.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+    <p1>adder!</p1>
+
+    <form action="adder.php" method="POST">
+    Number 1: <input type="number" name="num1"><br>
+    Number 2: <input type="number" name="num2"><br>
+    <input type="submit">
+    </form>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,17 +7,11 @@
 
     <h1>YOOOOOOOOOOOOO</h1>
 
-    <p1>adder!</p1>
-
-    <form action="adder.php" method="POST">
-    Number 1: <input type="number" name="num1"><br>
-    Number 2: <input type="number" name="num2"><br>
-    <input type="submit">
-    </form>
+    <a href="grade.php"><br>Portfolio Grade</a>
+    <a href="adder0.php"><br>Adder</a>
 
     <!-- php code can be put anywhere, you can have multiple as well -->
     <?php
-        echo ("helloooooo");
 
         // &emsp; makes spaces, like in java and html, &ensp; for small space
         echo ("<br>oooooooooooo<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&ensp;&ensp;#&emsp;&emsp;&ensp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>oooooooooooo<br>");
