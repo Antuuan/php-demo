4fe9da5d1233a8d2b6c22a2f1ed3daf2ebff30ab
Antuuan <antonycheng2007@gmail.com>
2024-10-15 09:21:53 +0100

    oisjF

---
+++ adder.php
@@ -0,0 +1,14 @@
+<html>
+
+    <body>
+
+        <?php
+
+            // $_ means variable, POST is the form thats been sent over form the previous page
+            echo($_POST["num2"]+$_POST["num1"]);
+        ?>
+
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+    <h1>YOOOOOOOOOOOOO</h1>
+
+    <p1>adder!</p1>
+
+    <form action="adder.php" method="POST">
+    Number 1: <input type="number" name="num1"><br>
+    Number 2: <input type="number" name="num2"><br>
+    <input type="submit">
+    </form>
+
+    <!-- php code can be put anywhere, you can have multiple as well -->
+    <?php
+        echo ("helloooooo");
+
+        // &emsp; makes spaces, like in java and html, &ensp; for small space
+        echo ("<br>oooooooooooo<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&ensp;&ensp;#&emsp;&emsp;&ensp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>oooooooooooo<br>");
+    ?>
+
+</body>
+</html>
