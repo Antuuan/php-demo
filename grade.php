61846aee413bc0a1cf94c934fe0ee83b1421ac43
Antuuan <antonycheng2007@gmail.com>
2024-10-15 09:44:28 +0100

    sefi

---
+++ grade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+    <form action="grader.php" method="POST">
+    Analysis: <input type="text" name="Analysis"><br>
+    Design: <input type="text" name="Design"><br>
+    Implementation: <input type="text" name="Implementation"><br>
+    Evaluation: <input type="text" name="Evaluation"><br>
+    <input type="submit">
+    </form>
+
+</body>
+</html>
