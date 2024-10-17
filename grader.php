883a30d1ef8c015393250f947613093b6467c0cd
Antuuan <antonycheng2007@gmail.com>
2024-10-17 12:22:55 +0100

    srgdrsg

--- grader.php
+++ grader.php
@@ -3,7 +3,46 @@
 
     <?php
 
-        
+        $total=$_POST["Analysis"]+$_POST["Design"]+$_POST["Implementation"]+$_POST["Evaluation"];
+
+        switch ($total){
+            case $total>=80:
+                echo("score:".$total." grade:A*");
+            break;
+
+            case $total>=67:
+                echo("score:".$total." grade:A");
+            break;
+
+            case $total>=54:
+                echo("score:".$total." grade:B");
+            break;
+
+            case $total>=41:
+                echo("score:".$total." grade:C");
+            break;
+
+            case $total>=31:
+                echo("score:".$total." grade:D");
+            break;
+
+            case $total>=22:
+                echo("score:".$total." grade:E");
+            break;
+
+            case $total>=13:
+                echo("score:".$total." grade:F");
+            break;
+
+            case $total>=4:
+                echo("score:".$total." grade:G");
+            break;
+
+            case $total>=0:
+                echo("score:".$total." grade:U");
+            break;
+            
+        }
 
     ?>
 
